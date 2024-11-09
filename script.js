1979dfc28cbddb96d915dad936b787dfdb1b23bb
Natalie Vykoukalova <naty.vykoukalova@gmail.com>
2024-11-09 14:11:59 +0100

    mala pismena

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const jmeno = prompt("Zadejte vaše jméno (bez diakritiky):")
 const prijmeni = prompt("Zadejte vaše příjmení (bez diakritiky):")
 const domena = "@fit.cvut.cz"
 
-const uzivatelskeJmeno = prijmeni.trim().slice(0,5) + jmeno.trim().slice(0,3)
+const uzivatelskeJmeno = prijmeni.trim().slice(0,5).toLowerCase() + jmeno.trim().slice(0,3).toLowerCase()
 
 document.body.innerHTML = ` Vaše FIT emailová adresa je: ${uzivatelskeJmeno + domena}
 `
